5d0c0d8c6eaae25b38adcfd3e644d57a80f2847f
yongsin0129 <yongsin0129@gmail.com>
2022-05-23 20:35:09 +0800

    feat : clean collection before create dummy data

--- models/seeds/addDummyData.js
+++ models/seeds/addDummyData.js
@@ -5,17 +5,28 @@ function createSeedSuccessfully () {
   console.log('create dummyData Successfully')
 }
 
-dummyData.forEach(data => {
-  const restaurant = new Restaurant({
-    name: data.name,
-    name_en: data.name_en,
-    category: data.category,
-    image: data.image,
-    location: data.location,
-    phone: data.phone,
-    google_map: data.google_map,
-    rating: data.rating,
-    description: data.description
+function createDummyData () {
+  dummyData.forEach(data => {
+    const restaurant = new Restaurant({
+      name: data.name,
+      name_en: data.name_en,
+      category: data.category,
+      image: data.image,
+      location: data.location,
+      phone: data.phone,
+      google_map: data.google_map,
+      rating: data.rating,
+      description: data.description
+    })
+    restaurant.save()
+  }, createSeedSuccessfully())
+}
+
+// 先將 collection 清空再創造新的資料
+Restaurant.deleteMany()
+  .then(() => {
+    createDummyData()
   })
-  restaurant.save()
-}, createSeedSuccessfully())
+  .catch(error => {
+    console.log(error)
+  })
