4900acc5c7a23a7e825634079a2bcca908cd7fca
yongsin0129 <yongsin0129@gmail.com>
2022-06-12 19:16:53 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
+const users = require('./modules/users')
 const noFound404 = require('./modules/noFound404')
 
-router.use('/', home)
 router.use('/Restaurants', restaurants)
 router.use('/search', search)
+router.use('/users', users)
+router.use('/', home)
 // 查無資料時的路由
 router.use('*', noFound404)
 
