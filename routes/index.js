1cc684a3c775fee8eaba124dd861955b0ed7475c
yongsin0129 <yongsin0129@gmail.com>
2022-06-12 19:56:32 +0800

    feat: add user authentication in restaurant and home routers

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 const noFound404 = require('./modules/noFound404')
 
-router.use('/Restaurants', restaurants)
-router.use('/search', search)
+router.use('/Restaurants', authenticator, restaurants)
+router.use('/search', authenticator, search)
 router.use('/users', users)
-router.use('/', home)
+router.use('/', authenticator, home)
 // 查無資料時的路由
 router.use('*', noFound404)
 
